3772d069181511e4dc5331937224d952194e2bb2
bleakwave <bleakwave@gmail.com>
2022-08-11 11:23:39 +0800

    psa qr code modifications

--- src/main/java/io/mosip/digitalcard/service/impl/PDFCardServiceImpl.java
+++ src/main/java/io/mosip/digitalcard/service/impl/PDFCardServiceImpl.java
@@ -3,6 +3,7 @@ package io.mosip.digitalcard.service.impl;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.mosip.biometrics.util.ConvertRequestDto;
 import io.mosip.biometrics.util.face.FaceDecoder;
 import io.mosip.digitalcard.constant.*;
@@ -14,7 +15,6 @@ import io.mosip.digitalcard.exception.DigitalCardServiceException;
 import io.mosip.digitalcard.exception.IdentityNotFoundException;
 import io.mosip.digitalcard.repositories.DigitalCardTransactionRepository;
 import io.mosip.digitalcard.util.*;
-import io.mosip.digitalcard.websub.WebSubSubscriptionHelper;
 import io.mosip.kernel.core.cbeffutil.spi.CbeffUtil;
 import io.mosip.kernel.core.exception.ServiceError;
 import io.mosip.kernel.core.http.RequestWrapper;
@@ -30,17 +30,33 @@ import io.mosip.vercred.CredentialsVerifier;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.bouncycastle.crypto.CryptoException;
+import org.bouncycastle.crypto.Signer;
+import org.bouncycastle.crypto.params.Ed25519PrivateKeyParameters;
+import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
+import org.bouncycastle.crypto.signers.Ed25519Signer;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -126,7 +142,8 @@ public class PDFCardServiceImpl implements CardGeneratorService {
 	@Value("${mosip.digitalcard.templateTypeCode:RPR_UIN_CARD_TEMPLATE}")
 	private String uinCardTemplate;
 
-
+	private static String piktyur;
+	
 	@Autowired
 	private ObjectMapper objectMapper;
 
@@ -134,6 +151,10 @@ public class PDFCardServiceImpl implements CardGeneratorService {
 	}
 
 
+    static {
+        nu.pattern.OpenCV.loadShared();
+        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
+    }
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -179,7 +200,14 @@ public class PDFCardServiceImpl implements CardGeneratorService {
 							DigitalCardServiceErrorCodes.TEM_PROCESSING_FAILURE.getErrorCode(),DigitalCardServiceErrorCodes.TEM_PROCESSING_FAILURE.getErrorMessage());
 				}
 				pdfbytes = generateUinCard(uinArtifact, password);
+				//to be deleted when making Jar
+				InputStreamResource resource = new InputStreamResource(new ByteArrayInputStream(pdfbytes));
+                File pdfFile = new File("src/main/resources/uin.pdf");
+                OutputStream os = new FileOutputStream(pdfFile);
+                os.write(pdfbytes);
+                os.close();
 			}
+			
 
 		}
 
@@ -210,11 +238,72 @@ public class PDFCardServiceImpl implements CardGeneratorService {
 	private boolean setQrCode(String qrString, Map<String, Object> attributes,boolean isPhotoSet)
 			throws IOException, QrcodeGenerationException {
 		boolean isQRCodeSet = false;
+						
 		JSONObject qrJsonObj = objectMapper.readValue(qrString, JSONObject.class);
+		JSONObject test = new JSONObject();
+		JSONObject sb = new JSONObject();
+		
 		if(isPhotoSet) {
 			qrJsonObj.remove("biometrics");
 		}
-		byte[] qrCodeBytes = qrCodeGenerator.generateQrCode(qrJsonObj.toString(), QrVersion.V30);
+
+		String compressedBase64 = null;
+		if (piktyur != null) {
+			compressedBase64 = generateFace();
+			if(compressedBase64 != null) {
+				test.put("img", compressedBase64);
+			}
+		}
+		else {
+			System.out.println("WAlang piktuyr");
+		}
+		
+		String fn = removeSide(qrJsonObj.get("fn").toString());
+		String mn = removeSide(qrJsonObj.get("mn").toString());
+		String ln = removeSide(qrJsonObj.get("ln").toString());
+		String bf = removeSideBf(qrJsonObj.get("BF").toString());
+		String sx = removeSideGen(qrJsonObj.get("gen").toString());
+		String dob2 = removeSideDate(qrJsonObj.get("dob").toString());
+		String pob = removeSide(qrJsonObj.get("pob").toString());
+		sb.put("PCN", qrJsonObj.get("PCN"));
+				
+		String forSigning = "{ \"i\": \"PSA\"," +
+				" \"sb\": {   \"sf\": \"\",   " +
+				"\"ln\": \""+ln+"\",   "+
+				"\"fn\": \""+fn+"\",   "+
+				"\"mn\": \""+mn+"\",   "+
+				"\"s\": \""+sx+"\",   "+
+				"\"BF\": \""+bf+"\",   "+
+				"\"DOB\": \""+dob2+"\",   "+
+				"\"POB\": \""+pob+"\",   "+
+				"\"PCN\": \""+qrJsonObj.get("PCN")+"\" },"+
+				"\"img\": \""+compressedBase64+"\"}";
+
+		System.out.println(forSigning);
+		String signedSignature = null;
+		try {
+			signedSignature = digitallySignSignature(forSigning);
+			System.out.println("SIGNATURA : " + signedSignature);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		String forQR = "{ \"i\": \"PSA\"," +
+				" \"sb\": {   \"sf\": \"\",   " +
+				"\"ln\": \""+ln+"\",   "+
+				"\"fn\": \""+fn+"\",   "+
+				"\"mn\": \""+mn+"\",   "+
+				"\"s\": \""+sx+"\",   "+
+				"\"BF\": \""+bf+"\",   "+
+				"\"DOB\": \""+dob2+"\",   "+
+				"\"POB\": \""+pob+"\",   "+
+				"\"PCN\": \""+qrJsonObj.get("PCN")+"\" },"+
+				"\"img\": \""+compressedBase64+"\","+
+				"\"si\": \""+signedSignature+"\"}";
+		
+		byte[] qrCodeBytes = qrCodeGenerator.generateQrCode(forQR, QrVersion.V20);
+//		byte[] qrCodeBytes = qrCodeGenerator.generateQrCode(test.toString(), QrVersion.V25);
 		if (qrCodeBytes != null) {
 			String imageString = Base64.encodeBase64String(qrCodeBytes);
 			attributes.put(QRCODE, "data:image/png;base64," + imageString);
@@ -248,6 +337,7 @@ public class PDFCardServiceImpl implements CardGeneratorService {
 			if (photoByte != null) {
 				byte[] data = FaceDecoder.convertFaceISOToImageBytes(convertRequestDto);
 				String encodedData = StringUtils.newStringUtf8(Base64.encodeBase64(data, false));
+				piktyur = encodedData;
 				attributes.put(APPLICANT_PHOTO, "data:image/png;base64," + encodedData);
 				isPhotoSet = true;
 			}
@@ -357,6 +447,138 @@ public class PDFCardServiceImpl implements CardGeneratorService {
 		logger.debug("UinCardGeneratorImpl::generateUinCard()::exit");
 
 		return pdfSignatured;
+	}
+	
+	private String generateFace() throws IOException {
+		if (piktyur == null) {
+			return "No Image";
+		}
+		
+		byte[] sData = Base64.decodeBase64(piktyur);
+		
+		Mat src = Imgcodecs.imdecode(new MatOfByte(sData), Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);
+		int image_x_coor = src.width() / 2;
+		int image_y_coor = src.height() / 2;
+		
+		String xmlFile = "src/main/resources/haarcascade_frontalface_default.xml";
+		CascadeClassifier classifier = new CascadeClassifier(xmlFile);
+		
+		MatOfRect faceDetections = new MatOfRect();
+		classifier.detectMultiScale(src, faceDetections);
+		
+		Rect rect_crop = null;
+		
+		Rect[] faces = faceDetections.toArray();
+		Rect found_face = null;
+		int faces_length = faces.length;
+		
+		if(faces.length == 0) {
+			return "No Image";
+		}else if(faces_length == 1) {
+			found_face = faces[0];
+		}else {
+			List<Double> list_centers = new ArrayList<>();
+			for(Rect rect: faces) {
+				int x = rect.x;
+				int y = rect.y;				
+				double x_distance = Math.pow(x - image_x_coor, 2);
+				double y_distance = Math.pow(y - image_y_coor, 2);
+				double distance = Math.sqrt(x_distance + y_distance);
+				list_centers.add(distance);
+			}
+			double min_val = Collections.min(list_centers);
+			int index_min_val = list_centers.indexOf(min_val);
+			found_face = faces[index_min_val];
+		}
+		
+		rect_crop = new Rect(found_face.x, found_face.y,found_face.x + found_face.width, found_face.y + found_face.height);		
+		
+		try {
+			Mat image_row = new Mat(src, rect_crop);
+			
+			Imgproc.cvtColor(image_row, image_row, Imgproc.COLOR_RGB2GRAY);
+
+			Imgproc.resize(image_row, image_row, new Size(45,58));
+			MatOfInt map = new MatOfInt(Imgcodecs.IMWRITE_WEBP_QUALITY, 30);
+			
+			MatOfByte mem = new MatOfByte();
+			Imgcodecs.imencode(".webp", image_row, mem, map);
+
+			try {
+				String compressedB64 = Base64.encodeBase64String(mem.toArray());
+				return compressedB64;
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	private String removeSide(String s) {
+		String codeString = s.replace("[{language=eng, value=","");
+		String codeString2 = codeString.replace("}]", "");
+		return codeString2;
+	}
+	
+	private String removeSideDate(String s) {
+		String s1 = s.replace("/", "-");
+		String s2 = s1.replace("Date of Birth ", "");
+		return s2;
+	}
+	
+	private String removeSideGen(String s) {
+		String codeString = s.replace("[{language=eng, value=","");
+		String codeString2 = codeString.replace("}]", "");
+		String codeString3 = null;
+		if(codeString2 == "Female") {
+			codeString3 = "f";
+		}
+		else {
+			codeString3 = "m";
+		}
+		
+		return codeString3;
+	}
+	
+	private String removeSideBf(String bio) {
+		String rString = null;
+		try {
+			String lrString = bio.substring(bio.indexOf("{rank=")+6,bio.indexOf(", subType"));
+			String rrBString = bio.substring(bio.indexOf(", {rank=")+8,bio.length());
+			String rrString = rrBString.substring(0,rrBString.indexOf(", subType"));
+			return rString = "[" + lrString + "," + rrString + "]";
+		}
+		catch (Exception f) {
+			f.printStackTrace();
+		}
+		return rString;
+	}
+	
+	
+	private String digitallySignSignature(String stringToSign) throws CryptoException {
+        var msg = stringToSign.getBytes(StandardCharsets.UTF_8);
+
+        var privateKeyBytes = Base64.decodeBase64("dazPBzb3e32NKQxTUm/HsEKMfHaCqFM+gf2jCQk0SCyBTlmwEgfcDV5QD/Ml9wX3WeYMQy5pMfjX+eDxvODNZA==");
+        var publicKeyBytes = Base64.decodeBase64("gU5ZsBIH3A1eUA/zJfcF91nmDEMuaTH41/ng8bzgzWQ=");
+
+        var privateKey = new Ed25519PrivateKeyParameters(privateKeyBytes, 0);
+        var publicKey = new Ed25519PublicKeyParameters(publicKeyBytes, 0);
+
+        // Generate new signature
+        Signer signer = new Ed25519Signer();
+        signer.init(true, privateKey);
+        signer.update(msg, 0, msg.length);
+        byte[] signature = signer.generateSignature();
+        var actualSignature = Base64.encodeBase64String(signature);
+        
+		return actualSignature;
 	}
 }
 	
